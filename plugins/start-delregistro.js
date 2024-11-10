0189d50373ebb3c942903492297b635ceadf5c04
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2024-11-10 19:50:03 -0300

    ಠ⁠,⁠_⁠｣⁠ಠ

--- plugins/start-delregistro.js
+++ plugins/start-delregistro.js
@@ -1,13 +1,13 @@
-import { createHash } from 'crypto' 
-let handler = async (m, { conn, text, usedPrefix }) => {
+let handler = async (m, { conn, text }) => {
+
 let user = global.db.data.users[m.sender]
-let sn = createHash('md5').update(m.sender).digest('hex').slice(0, 6)	 
-if (!text) return conn.reply(m.chat, `🔖 *Falta su número de serie.*\n\n> 🩷 Puede usar el comando *${usedPrefix}numserie* para obtener su número de serie.`, m)
-if (text !== sn) return conn.reply(m.chat, `💔 *Número de serie incorrecto.*\n\n> ✨ Puede usar el comando *${usedPrefix}numserie* para obtener su número de serie.`, m)
+
 user.registered = false
-m.react('✅') 
-conn.sendMessage(m.chat, { text: `${dis}*Registro eliminado de ${packname}*\n\n> _Si desea hacer un nuevo registro use el comando._\n*${usedPrefix}verificar* \`nombre.edad\``, ...fake }, { quoted: m })
+return conn.reply(m.chat, `*『✅』 Usted Ya No Está En Mi Base De Datos*`, m, rcanal)
+
 }
-handler.command = /^(unreg|delregistro)$/i
+handler.help = ['unreg']
+handler.tags = ['rg']
+handler.command = ['unreg']
 handler.register = true
 export default handler
