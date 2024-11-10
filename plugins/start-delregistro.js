c497b6f90124548b557860540b7cb1f1ef664328
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2024-11-10 19:52:07 -0300

    Actualizar start-delregistro.js

--- plugins/start-delregistro.js
+++ plugins/start-delregistro.js
@@ -3,11 +3,11 @@ let handler = async (m, { conn, text }) => {
 let user = global.db.data.users[m.sender]
 
 user.registered = false
-return conn.reply(m.chat, `*『✅』 Usted Ya No Está En Mi Base De Datos*`, m, rcanal)
+m.reply(`*『✅』Ya no estas registrado en este Bot*`)
 
 }
 handler.help = ['unreg']
-handler.tags = ['rg']
-handler.command = ['unreg']
+handler.tags = ['xp']
+handler.command = /^unreg(ister)?$/i
 handler.register = true
 export default handler
