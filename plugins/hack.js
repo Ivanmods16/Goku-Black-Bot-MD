c422f113d37fa7fa15e37eff58bf8152a76a5b64
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2024-11-26 13:07:23 -0300

    Yagami-Bot-MD 🐬

---
+++ plugins/hack.js
@@ -0,0 +1,14 @@
+//Código creado por elias1524
+
+var handler = async m => 
+
+conn.reply(m.chat, `*TODOS USTEDES SERA HACKEADOS DIGAN SOY GAY Y NO SERAN HACKEADOS*`, m, rcanal, )
+
+handler.help = ['hack']
+handler.tags = ['hack']
+handler.command = /^hack$/i
+
+handler.limit = true
+handler.register = true
+
+export default handler 
