d9515d142781b613ab2b1e571e11c5ee6180b106
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2024-11-27 16:36:57 -0300

    Yagami-Bot-MD

--- plugins/Descargas•mediafire.js
+++ plugins/Descargas•mediafire.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 if (!text) throw m.reply(`🍟 Ingresa un link de mediafire\n*🚩 Ejemplo:* ${usedPrefix}${command} https://www.mediafire.com/file/2v2x1p0x58qomva/WhatsApp_Messenger_2.24.21.8_beta_By_WhatsApp_LLC.apk/file`);
 conn.sendMessage(m.chat, { react: { text: "🕒", key: m.key } });
-	let ouh = await fetch(`https://api.agatz.xyz/api/mediafire?url=${text}`)
+	let ouh = await fetch(`https://deliriussapi-oficial.vercel.app/api/mediafire?url=${args`)
   let gyh = await ouh.json()
 	await conn.sendFile(m.chat, gyh.data[0].link, `${gyh.data[0].nama}`, `*🚩 Nombre:* ${gyh.data[0].nama}\n*🚩 Tamaño:* ${gyh.data[0].size}\n*🚩 Extensión:* ${gyh.data[0].mime}`, m)
 	await conn.sendMessage(m.chat, { react: { text: '✅', key: m.key }})
