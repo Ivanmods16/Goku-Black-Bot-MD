e0bd173ee4833df561406d739761aaad8d1d1033
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2024-11-27 16:44:13 -0300

    Yagami-Bot-MD 🐬

--- plugins/Descargas•mediafire.js
+++ plugins/Descargas•mediafire.js
@@ -1,15 +1,15 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) throw m.reply(`🍟 Ingresa un link de mediafire\n*🚩 Ejemplo:* ${usedPrefix}${command} https://www.mediafire.com/file/2v2x1p0x58qomva/WhatsApp_Messenger_2.24.21.8_beta_By_WhatsApp_LLC.apk/file`);
+if (!text) throw m.reply(`Ingresa un link de mediafire\n*🌸 Ejemplo:* ${usedPrefix}${command} https://www.mediafire.com/file/2v2x1p0x58qomva/WhatsApp_Messenger_2.24.21.8_beta_By_WhatsApp_LLC.apk/file`);
 conn.sendMessage(m.chat, { react: { text: "🕒", key: m.key } });
-	let ouh = await fetch(`https://deliriussapi-oficial.vercel.app/api/mediafire?url=${args`)
+        let ouh = await fetch(`https://api.agatz.xyz/api/mediafire?url=${text}`)
   let gyh = await ouh.json()
-	await conn.sendFile(m.chat, gyh.data[0].link, `${gyh.data[0].nama}`, `*🚩 Nombre:* ${gyh.data[0].nama}\n*🚩 Tamaño:* ${gyh.data[0].size}\n*🚩 Extensión:* ${gyh.data[0].mime}`, m)
-	await conn.sendMessage(m.chat, { react: { text: '✅', key: m.key }})
+        await conn.sendFile(m.chat, gyh.data[0].link, `${gyh.data[0].nama}`, `*🌹 Nombre:* ${gyh.data[0].nama}\n*🪷 Tamaño:* ${gyh.data[0].size}\n*🍒 Extensión:* ${gyh.data[0].mime}\n> ৎ୭࠭͢𝒴𝓊𝓀𝒾_𝒮𝓊𝑜𝓊-𝐵𝑜𝑡𝐭ⷭ𓆪͟͞ `, m)
+        await conn.sendMessage(m.chat, { react: { text: '✅', key: m.key }})
 }
-handler.help = ['mediafire *<link>*']
-handler.tags = ['downloader']
+handler.help = ['mediafire']
+handler.tags = ['descargas']
 handler.command = /^(mediafire|mf)$/i
 handler.premium = false
 handler.register = true
