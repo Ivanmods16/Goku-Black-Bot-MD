f12f1bcef52a7e7fabc13cef64a1e5556e2796d9
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2024-12-04 19:16:16 -0300

    Actualizar prueben.js

--- plugins/prueben.js
+++ plugins/prueben.js
@@ -1,55 +1,31 @@
-import MessageType from '@whiskeysockets/baileys'
-import fetch from 'node-fetch'
-import fs from 'fs'
-
-var handler = async (m, { conn, text, usedPrefix, command }) => {
-
-if (!text) return conn.reply(m.chat, `🎌 *Ingrese el nombre de un repositorio de github*\n\nEjemplo, !${command} CuriosityBot-MD`, m, fake, )
-
-try {
-
-let res = await fetch(global.API('https://api.github.com', '/search/repositories', { q: text }))
-let json = await res.json()
-if (res.status !== 200) throw json
-let str = json.items.map((repo, index) => {
-return `
-⬡ *Resultado:* ${1 + index}
-⬡ *Enlace:* ${repo.html_url}
-⬡ *Creador:* ${repo.owner.login}
-⬡ *Nombre:* ${repo.name}
-⬡ *Creado:* ${formatDate(repo.created_at)}
-⬡ *Actualizado:* ${formatDate(repo.updated_at)}
-⬡ *Visitas:* ${repo.watchers}
-⬡ *Bifurcado:* ${repo.forks}
-⬡ *Estrellas:* ${repo.stargazers_count}
-⬡ *Issues:* ${repo.open_issues}
-⬡ *Descripción:* ${repo.description ? `${repo.description}` : 'Sin Descripción'}
-⬡ *Clone:* ${repo.clone_url}
-`.trim()}).join('\n\n─────────────────\n\n')
-
-let img = await (await fetch(json.items[0].owner.avatar_url)).buffer()
-await conn.sendUrl(m.chat, str, m, { externalAdReply: { mediaType: 1, renderLargerThumbnail: true, thumbnail: img, thumbnailUrl: img, title: 'Resultados Encontrados 🔎',
-}
-})
-
-} catch {
-conn.reply(m.chat, '🚩 *Ocurrió un fallo*', m, fake, )
-}
-
-}
-handler.help = ['githubsearch']
-handler.tags = ['internet']
-handler.command = /^(githubsearch)$/i
-
-handler.register = true
-
-export default handler 
+import fetch from 'node-fetch';
+const handler = async (m, {conn, text, usedPrefix, command}) => {
+  if (!text) throw `*[❗] Ingresa un texto para buscar, ejemplo: ${usedPrefix + command} GokuBot-MD*`;
+  const res = await fetch(global.API('https://api.github.com', '/search/repositories', {
+    q: text,
+  }));
+  const json = await res.json();
+  if (res.status !== 200) throw json;
+  //const imagen = await conn.getFile(json.items[0].owner.avatar_url).data
+  const str = json.items.map((repo, index) => {
+  return `
+*${1 + index}. ${repo.full_name}${repo.fork ? ' (fork)' : ''}*
+🔗 *Url:* ${repo.html_url}
+📅 *Creado el:* ${formatDate(repo.created_at)}
+🔄 *Actualizado el:* ${formatDate(repo.updated_at)}
+📥 *Clone:* $ git clone ${repo.clone_url}
+👁 ${repo.watchers} ◉ 🍴 ${repo.forks} ◉ ⭐ ${repo.stargazers_count} ◉ ❓ 
+${repo.description ? `📝 *Descripción:*\n${repo.description}` : ''}
+`.trim()}).join('\n\n◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦\n\n');
+  conn.sendMessage(m.chat, {text: str.trim()}, {quoted: m})
+//conn.sendMessage(m.chat, {text: str.trim(), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [m.sender], "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm2, "containsAutoReply": true, "mediaType": 1, "thumbnail": imagen, "mediaUrl": `https://www.atom.bio/theshadowbrokers-team`, "sourceUrl": `https://www.atom.bio/theshadowbrokers-team`}}}, {quoted: m});  
+};
+handler.help = ['githubs'];
+handler.tags = ['buscadores'];
+handler.command = /^(ghs|githubs|githubs|githubsearch|gits|gitsearch)$/i;
+export default handler;
 
 function formatDate(n, locale = 'es') {
-let d = new Date(n)
-return d.toLocaleDateString(locale, {
-weekday: 'long',
-day: 'numeric',
-month: 'long',
-year: 'numeric'
-}) }
+  const d = new Date(n);
+  return d.toLocaleDateString(locale, {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'});
+}
