d09a9b615ee4e6426d13ab6934f4f64ddf3f9e7b
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2024-12-04 18:58:22 -0300

    Goku-Black-Bot-MD 💥

--- plugins/Buscador•google.js
+++ plugins/Buscador•google.js
@@ -1,27 +1,36 @@
-import {googleIt} from '@bochilteam/scraper'
-import google from 'google-it'
-import axios from 'axios'
-let handler = async (m, { conn, command, args, usedPrefix }) => {
-const fetch = (await import('node-fetch')).default;
-const text = args.join` `
-if (!text) return conn.reply(m.chat, '🍟 Ingresa lo que deseas buscar en Google.', m, rcanal)
-conn.reply(m.chat, `🚩 Buscando Su Información...`, m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-title: packname,
-body: dev,
-previewType: 0, thumbnail: icons,
-sourceUrl: channel }}})
-const url = 'https://google.com/search?q=' + encodeURIComponent(text)
-google({'query': text}).then(res => {
-let teks = `🍟 *Resultado de* : ${text}\n\n`
-for (let g of res) {
-teks += `🐢 *Titulo ∙* ${g.title}\n🚩 *Info ∙* ${g.snippet}\n🔗 *Url ∙* ${g.link}\n\n`
-}
-conn.reply(m.chat, teks, m, rcanal)
-})
-}
-handler.help = ['google <búsqueda>']
-handler.tags = ['buscador']
-handler.command = ['google']
-handler.register = true 
-export default handler
+import axios from 'axios';
+
+let handler = async (m, { conn, command, args }) => {
+  const text = args.join(' ');
+  if (!text) return conn.reply(m.chat, '🌸 *Ingresa lo que deseas buscar junto al comando.*', m);
+
+  await m.react('🕓');
+  let img = 'https://i.ibb.co/P5kZNFF/file.jpg';
+
+  try {
+    const apiResponse = await axios.get(`https://api.dorratz.com/v2/google-search?q=${encodeURIComponent(text)}`);
+
+    if (apiResponse.data && apiResponse.data.results && Array.isArray(apiResponse.data.results) && apiResponse.data.results.length > 0) {
+      const results = apiResponse.data.results;
+
+      let teks = ` *乂  S E A R C H  -  G O O G L E*\n\n`;
+      for (let g of results) {
+        teks += `*${g.title}*\n${g.link}\n\n`;
+      }
+
+      conn.sendFile(m.chat, img, 'thumbnail.jpg', teks, m).then(() => m.react('✅'));
+    } else {
+      conn.reply(m.chat, '🔍 *No se encontraron resultados.*', m);
+    }
+  } catch (error) {
+    console.error(error);
+    conn.reply(m.chat, '⚠️ *Ocurrió un error al realizar la búsqueda: ' + error.message + '*', m);
+  }
+};
+
+handler.help = ['google *<texto>*'];
+handler.tags = ['buscador'];
+handler.command = /^google?$/i;
+handler.register = true;
+
+export default handler;
