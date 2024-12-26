fe286821897a50b6a8d3915ee4d20d88a8f51587
Eliasivan15 <159085113+Eliasivan@users.noreply.github.com>
2024-12-26 15:14:01 -0300

    Actualizar index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const require = createRequire(__dirname);
 const { name, description, author, version } = require(join(__dirname, './package.json'));
 const { say } = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
-say('Onyx\nBot-MD', {
+say('Fn\nBot-MD', {
 font: 'block',
 align: 'center',
 colors: ['red']
