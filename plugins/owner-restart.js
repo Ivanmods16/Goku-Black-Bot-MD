a1d87f2950ac9b91ea8a578b2b567211de3842e5
Dev-Rayo <159085113+Eliasivan@users.noreply.github.com>
2025-07-03 18:09:55 -0300

    Ludd

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -1,31 +1,19 @@
-import { spawn } from 'child_process'
+let handler = async (m, { conn, usedPrefix, command }) => {
 
-let handler = async (m, { conn, isROwner, text }) => {
-    if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
-    if (conn.user.jid == conn.user.jid) {
-        const { key } = await conn.sendMessage(m.chat, { text: `Reiniciando...` }, { quoted: m });
-
-        await delay(1000);
-        await conn.sendMessage(m.chat, { text: `Reiniciando...`, edit: key });
-
-        await delay(1000);
-        await conn.sendMessage(m.chat, { text: `Aguarde unos segundos más... 🔁`, edit: key });
-
-        await delay(1000);
-        await conn.sendMessage(m.chat, {
-            text: `Reinicio completo...☄︎`,
-            edit: key
-        });
-
-        process.send('reset');
-    } else throw 'eh'
+    try {
+        m.reply('「🔁」 Reiniciando El Bot....')
+        setTimeout(() => {
+            process.exit(0)
+        }, 3000) 
+    } catch (error) {
+        console.log(error)
+        conn.reply(m.chat, `${error}`, m)
+    }
 }
 
 handler.help = ['restart']
 handler.tags = ['owner']
-handler.command = ['restart', 'reiniciar']
+handler.command = ['restart', 'reiniciar'] 
 handler.rowner = true
 
 export default handler
-
-const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
