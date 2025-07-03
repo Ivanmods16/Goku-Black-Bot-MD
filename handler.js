c149b4204853e0e754aa8b9d081ca3549cdb973d
Ivanmods16 <eliasivan24q@gmail.com>
2025-07-03 19:14:53 -0300

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -255,8 +255,6 @@ m.exp += Math.ceil(Math.random() * 10)
 let usedPrefix
 
 const groupMetadata = (m.isGroup ? ((conn.chats[m.chat] || {}).metadata || await this.groupMetadata(m.chat).catch(_ => null)) : {}) || {}
-const participants = (m.isGroup ? groupMetadata.participants : []) || []
-const user = (m.isGroup ? participants.find(u => conn.decodeJid(u.id) === m.sender) : {}) || {}
 let numBot = (conn.user.lid || '').replace(/:.*/, '') || false
 const detectwhat2 = m.sender.includes('@lid') ? `${numBot}@lid` : conn.user.jid
 const bot = (m.isGroup ? participants.find(u => conn.decodeJid(u.id) == detectwhat2) : {}) || {}
