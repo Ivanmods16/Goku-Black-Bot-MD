8ff7c081d7e35ccaa414763a35b9c672f8c6499d
Dev-Rayo <159085113+Eliasivan@users.noreply.github.com>
2025-07-03 17:23:35 -0300

    LUFFY-BOT-MD

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -1,6 +1,4 @@
-import sharp from "sharp";
 import { promises as fs } from 'fs';
-import moment from "moment-timezone";
 
 const imgPath = "./src/catalogo.jpg";
 
@@ -9,39 +7,12 @@ let handler = async (m, { conn }) => {
         if (typeof m.react === "function") {
             await m.react("🍂");
         }
-
-        let menuText = `
-Comandos disponibles:
-
-• play
-• sticker
-• tiktok
-• fb
-• ig
-• kick
-• promote
-• depromote
-        `.trim();
-
-        let saludo = ucapan();
-        let txt = `🍄 ${saludo}, @${m.sender.split("@")[0]} !\n\n${menuText}`;
-        let mention = [m.sender];
-
-        let imager = await sharp(imgPath)
-            .resize(400, 400)
-            .jpeg()
-            .toBuffer();
-
         let img = await fs.readFile(imgPath);
-
         await conn.sendMessage(
             m.chat,
             {
                 image: img,
-                caption: txt,
-                jpegThumbnail: imager,
                 contextInfo: {
-                    mentionedJid: mention,
                     externalAdReply: {
                         title: "Luffy-Bot",
                         body: "Menú simple",
@@ -54,22 +25,10 @@ Comandos disponibles:
             },
             { quoted: m }
         );
-
     } catch (e) {
-        let txt = `🍄 ${ucapan()}, @${m.sender.split("@")[0]} !\n\nComandos disponibles:\n\n• play\n• sticker\n• tiktok\n• fb\n• ig\n• kick\n• promote\n• depromote`;
-        conn.reply(m.chat, txt, m, { mentions: [m.sender] });
-        conn.reply(m.chat, "❎ Error al mostrar el menú principal: " + e, m);
+        conn.reply(m.chat, "❎ Error al mostrar la imagen del menú: " + e, m);
     }
 };
 
 handler.command = ["menu", "help", "menú", "commands", "comandos", "?"];
 export default handler;
-
-function ucapan() {
-    const time = moment.tz("America/Los_Angeles").format("HH");
-    if (time >= 18) return "Good night.";
-    if (time >= 15) return "Good afternoon.";
-    if (time >= 10) return "Good afternoon.";
-    if (time >= 4) return "Good morning.";
-    return "Hello.";
-}
