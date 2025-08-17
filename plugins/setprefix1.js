2212e8d4b72e26ec40be5346e1ecc705b0942321
Dev-Rayo <eliasivan24q@gmail.com>
2025-08-17 12:51:25 -0300

    Crear setprefix1.js

---
+++ plugins/setprefix1.js
@@ -0,0 +1,16 @@
+const handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text) throw `ingrese el prefijo que quieres\n\nej: ${usedPrefix + command} #`;
+
+  let escapedPrefix = text.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+
+  global.prefix = new RegExp('^' + escapedPrefix);
+
+  await m.reply(`*Se actualizo el prefijo:* [ ${text} ]`);
+};
+
+handler.help = ['setprefix'].map((v) => v + ' [prefijo]');
+handler.tags = ['owner'];
+handler.command = /^(setprefix)$/i;
+handler.rowner = true;
+
+export default handler;
